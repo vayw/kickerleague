3c46c0359a7b7c80feb6ddc5729fa6f7ccbef670
Ivan Kirillov <i_kirillov@wargaming.net>
2019-12-08 02:22:29 +0300

    last matches api method

--- api/stats.go
+++ api/stats.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/vayw/kickerleague/database"
 	"github.com/vayw/kickerleague/game"
@@ -21,14 +19,24 @@ func matchResults(c *gin.Context) {
 	}
 
 	var data Data
+	var matches []models.Match
+
 	c.BindJSON(&data)
 
-	var matches []models.Match
+	result := make([]Result, data.Num)
 	database.DBCon.Limit(data.Num).Order("ts desc").Find(&matches)
 
-	for _, i := range matches {
-		fmt.Println(i)
+	var matchlineup []models.MatchData
+	for index, i := range matches {
+		result[index] = Result{}
+		result[index].Red = i.Red_score
+		result[index].Blue = i.Blue_score
+		result[index].Lineup = make([]game.Player, 4)
+		database.DBCon.Where("match_id = ?", i.ID).Find(&matchlineup)
+		for pindex, j := range matchlineup {
+			result[index].Lineup[pindex] = game.Player{j.PlayerID, j.Team, j.Position}
+		}
 	}
-	c.JSON(200, "OK")
 
+	c.JSON(200, result)
 }
